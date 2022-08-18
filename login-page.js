d7f563bbfb39171f3e8841ee135eaac0945a6ffe
Amrit Anand <96764622+hacamrit@users.noreply.github.com>
2022-08-18 15:47:13 +0530

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -3,14 +3,14 @@
 const loginButton = document.getElementById("login-form-submit");
 
 const loginErrorMsg = document.getElementById("login-error-msg");
-
+*/
 loginButton.addEventListener("click", (e) => {
 
     e.preventDefault();
 
     const username = loginForm.username.value;
 
-    const password = loginForm.password.value;*/
+    const password = loginForm.password.value;
 
     if (1) {
 
